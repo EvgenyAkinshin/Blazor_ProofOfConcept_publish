2d0660312f7294c38b354ae9ccd6f419aa0f4957
EvgenyAkinshin <eugene.akinshin@gmail.com>
2022-10-29 15:27:41 +0700

    1

--- _content/Aphalina.Drawing.Blazor/jsUtils.js
+++ _content/Aphalina.Drawing.Blazor/jsUtils.js
@@ -13,16 +13,32 @@ window.aphalinaUtils = {
     getWindowSize: function ()
     {
         return { width: window.innerWidth, height: window.innerHeight, devicePixelRatio: window.devicePixelRatio };
-    }
+    },
+
+    //measureText: function (textHtmls)
+    //    var resultList = [];
+    //    if (!this.measureElement)
+    //    {
+    //        this.measureElement = document.getElementById('measureElement');
+    //    }
+
+    //    for (var textHtml in textHtmls)
+    //    {
+    //        this.measureElement.innerHTML = textHtml;
+    //        resultList.push({
+    //            width: this.measureElement.clientWidth,
+    //            height: this.measureElement.clientHeight
+    //        });
+    //    }
+
+    //    return resultList;
+    //}
 
 };
 
 function reportWindowSizeChanged() {
 
     DotNet.invokeMethodAsync('Aphalina.Drawing.Blazor', 'ReportWindowSizeChanged', { width: window.innerWidth, height: window.innerHeight, devicePixelRatio: window.devicePixelRatio });
-
-    //heightOutput.textContent = window.innerHeight;
-    //widthOutput.textContent = window.innerWidth;
 }
 
 window.onresize = reportWindowSizeChanged;
